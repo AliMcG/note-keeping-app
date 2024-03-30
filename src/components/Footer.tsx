2d6281f972a3c82dba06ad38bcdf1da75d7d916e
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-03-30 09:59:53 +0000

    Make footer sticky

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React from "react";
 
 export default function Footer() {
-  const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
   return (
-    <footer className="flex flex-1 w-full justify-center fixed bottom-5">
+    <footer className="sticky top-[100vh] flex w-full flex-1 justify-center">
       <p className="text-white">Copyright © {year}</p>
     </footer>
-  )
+  );
 }
