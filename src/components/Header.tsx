abdbc3772ab71c5506f8c4094a1813c765c4c6bc
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-03-30 09:14:20 +0000

    Increase z index of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import AuthenticationButton from "./AuthenticationButton";
 
 export default function Header() {
   return (
-    <header className="bg-[#75988c] flex justify-between fixed top-0 w-full p-2 font-bold">
+    <header className="bg-[#75988c] flex justify-between fixed top-0 w-full p-2 font-bold z-20">
       <h1 className="text-lg md:text-2xl lg:text-3xl flex items-center ml- gap-4 font-monts">
         <EmojiObjectsIcon className="text-yellow-100 h-10 w-10"/>
         Note Keeping App
