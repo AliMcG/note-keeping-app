83d9f3ddd98f262273a0e74ae8eda2380dd2606f
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-03-30 10:00:30 +0000

    file prettier

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
 import AuthenticationButton from "./AuthenticationButton";
 
-
 export default function Header() {
   return (
-    <header className="bg-[#75988c] flex justify-between fixed top-0 w-full p-2 font-bold z-20">
-      <h1 className="text-lg md:text-2xl lg:text-3xl flex items-center ml- gap-4 font-monts">
-        <EmojiObjectsIcon className="text-yellow-100 h-10 w-10"/>
+    <header className="fixed top-0 z-50 flex w-full justify-between bg-[#75988c] p-2 font-bold">
+      <h1 className="ml- flex items-center gap-4 font-monts text-lg md:text-2xl lg:text-3xl">
+        <EmojiObjectsIcon className="h-10 w-10 text-yellow-100" />
         Note Keeping App
       </h1>
       <AuthenticationButton />
