1a9b233b5b5abb081c5eef7094ba29d9ce620e61
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-04-02 20:04:44 +0100

    update tailwind config with -primary et al

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,14 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        primary: "hsl(159, 15%, 53%)",
+        "primary-foreground": "white",
+        secondary: "",
+        "secondary-foreground": "",
+        "primary-text": "hsl(229, 84%, 5%)",
+        "secondary-text": "",
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
         monts: ['var(--font-monts)'],
