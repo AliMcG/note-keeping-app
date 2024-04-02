1229d6f3f29eb666b98df225ba5ffc8d6ed99667
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-04-02 20:15:57 +0100

    Update tailwind classes to use primary, secondary et al

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import AuthenticationButton from "./AuthenticationButton";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 z-50 flex w-full justify-between bg-[#75988c] p-2 font-bold">
-      <h1 className="ml- flex items-center gap-4 font-monts text-lg md:text-2xl lg:text-3xl">
+    <header className="fixed top-0 z-50 flex w-full justify-between bg-primary p-2 font-bold">
+      <h1 className="ml- flex items-center gap-4 font-monts text-lg md:text-2xl lg:text-3xl text-primary-text">
         <EmojiObjectsIcon className="h-10 w-10 text-yellow-100" />
         Note Keeping App
       </h1>
--- src/components/note.tsx
+++ src/components/note.tsx
@@ -15,7 +15,7 @@ export default function Note({ note, deleteFunction }: NoteProps) {
   const { title, description, id } = note;
   return (
     <div className="relative float-left mx-4 my-4 w-60 h-40 rounded-lg bg-white p-4 shadow-2xl font-monts">
-      <h1 className="text-lg font-semibold text-slate-950">{title}</h1>
+      <h1 className="text-lg font-semibold text-primary-text">{title}</h1>
       <p>{description}</p>
       {deleteFunction && <button
         className="absolute bottom-1 right-1 bg-slate-200 h-9 w-14 cursor-pointer rounded-lg border-none bg-transparent text-gray-600 outline-none hover:bg-gray-300"
