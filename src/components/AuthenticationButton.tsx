92407ba10541c311469ea2a86720dd44c0d9c6ff
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-04-18 22:13:13 +0100

    Use new Button component

--- src/components/AuthenticationButton.tsx
+++ src/components/AuthenticationButton.tsx
@@ -1,19 +1,23 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import { Button } from "rawsome-components";
 
-export default function AuthShowcase() {
+export default function AuthenticationButton() {
   const { data: sessionData } = useSession();
 
   return (
-    <div className="flex justify-evenly items-center text-sm md:text-lg gap-1 md:gap-4 font-monts flex-col-reverse md:flex-row text-text-primary">
+    <div className="text-text-primary flex flex-col-reverse items-center justify-evenly gap-1 font-monts text-sm md:flex-row md:gap-4 md:text-lg">
       <p className="text-center">
         {sessionData && <span>{sessionData.user?.name}</span>}
       </p>
-      <button
-        className="rounded-full bg-white px-2 md:px-4 py-1.5 font-semibold no-underline transition hover:bg-white/60 items-center"
-        onClick={sessionData ? () => void signOut() : () => void signIn("google")}
+      <Button
+        intent={"secondary"}
+        className="items-center rounded-full  px-2 py-1.5 font-semibold   md:px-4"
+        onClick={
+          sessionData ? () => void signOut() : () => void signIn("google")
+        }
       >
         {sessionData ? "Sign out" : "Sign in"}
-      </button>
+      </Button>
     </div>
   );
 }
