96b2c81bd2ccbd508f63b36b7220ba95bf3f9021
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2024-04-18 22:12:50 +0100

    Remove odd bug tailwind classNames

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="sticky top-[110vh] md:fixed md:bottom-5 md:top-0 flex w-full flex-1 justify-center">
+    <footer className=" flex w-full flex-1 justify-center">
       <p className="text-white">Copyright © {year}</p>
     </footer>
   );
